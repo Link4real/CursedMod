cc9a414b69421b123ede650895f9a16569d544d1
Link4Real <simon@die-duerrs.de>
2021-08-05 23:28:05 +0200

    yay

--- src/main/java/com/link/cursed/mod/Main.java
+++ src/main/java/com/link/cursed/mod/Main.java
@@ -51,7 +51,6 @@ public static final String MOD_ID = "cursed";
                 stacks.add(new ItemStack(RegisterHalfDiamondArmor.DIAMOND_TOP));
                 stacks.add(new ItemStack(RegisterHalfDiamondArmor.DIAMOND_BERET));
                 stacks.add(new ItemStack(ModItems.DIAMOND_PUFFERFISH));
-                stacks.add(new ItemStack(ModItems.TURTLE_CAKE));
             })
             .build();
     // ...
